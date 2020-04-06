9da4c48292470a463a1c016addd7c31c3e16101c
Arthur Dvurechenskiy <gibbrich@yandex.ru>
2020-04-06 11:04:02 +0300

    Add handle 426 http error

--- data/src/main/java/com/github/gibbrich/breakingnews/data/repository/ArticlesDataRepository.kt
+++ data/src/main/java/com/github/gibbrich/breakingnews/data/repository/ArticlesDataRepository.kt
@@ -5,6 +5,7 @@ import com.github.gibbrich.breakingnews.core.repository.ArticlesRepository
 import com.github.gibbrich.breakingnews.data.converter.ArticlesConverter
 import com.github.gibbrich.breakingnews.data.api.Api
 import com.github.gibbrich.breakingnews.data.db.AppDatabase
+import retrofit2.HttpException
 
 class ArticlesDataRepository(
     private val api: Api,
@@ -18,14 +19,19 @@ class ArticlesDataRepository(
         private const val API_KEY = "3dc15b6483c24af8a664397254824bc5"
     }
 
-    /**
-     * todo - handle error case:
-     * {"status":"error","code":"maximumResultsReached","message":"You have requested too many results. Developer accounts are limited to a max of 100 results. You are trying to request results 100 to 120. Please upgrade to a paid plan if you need more results."}
-     * Http error 426
-     */
     override suspend fun getTopHeadLines(page: Int, articlesInPage: Int): List<Article> {
-        return api.getTopHeadlines(API_KEY, page, articlesInPage)
-            .articles
-            .map(ArticlesConverter::fromNetwork)
+        return try {
+            api.getTopHeadlines(API_KEY, page, articlesInPage)
+                .articles
+                .map(ArticlesConverter::fromNetwork)
+        } catch (e: Exception) {
+            // http 426 error means, that we are out of request limit for free subscription,
+            // so just return empty list
+            if (e is HttpException && e.code() == 426) {
+                return emptyList()
+            } else {
+                throw e
+            }
+        }
     }
 }
